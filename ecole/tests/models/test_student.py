56a0461ef2d6a0fb7f38f35f590b7a619b460ed1
Jonathan M. <neodev7440@gmail.com>
2026-04-14 13:09:27 +0200

    ajout des fixture + test fname et lname dans le str

--- ecole/tests/models/test_student.py
+++ ecole/tests/models/test_student.py
@@ -1,3 +1,23 @@
+from unittest.mock import MagicMock
 
+import pytest
+
+from ecole.models.student import Student
+
+#-----------------------les fixtrues--------------------
+@pytest.fixture
+def student():
+    return Student(first_name="Paul", last_name="Dubois", age=12)
+
+@pytest.fixture
+def course():
+    mock = MagicMock()
+    mock.students_taking_it =[]
+    return mock
+
+#------------------test str----------------------
+def test_should_contains_first_name_and_last_name(student):
+    assert "Paul" in str(student)
+    assert "Dubois" in str(student)
 
 
