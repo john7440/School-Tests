81fecbb1a5fa30ec76bd2927c45af1c0a2c489d0
Jonathan M. <neodev7440@gmail.com>
2026-04-14 14:51:14 +0200

    ajout d'un test pour ajouter de multiples cours a un teacher

--- ecole/tests/models/test_teacher.py
+++ ecole/tests/models/test_teacher.py
@@ -27,6 +27,15 @@ def test_add_course_should_set_teacher_on_course(teacher,course):
     teacher.add_course(course)
     assert course.teacher == teacher
 
+def test_add_multiple_courses_should_all_have_teacher_set(teacher,course):
+    courses = [MagicMock(),MagicMock(),MagicMock()]
+    for c in courses:
+        c.teacher = None
+        teacher.add_course(c)
+    for c in courses:
+        assert c.teacher == teacher
+
+
 #-----------------------test str---------------------------
 def test_str_should_match_expected_format_without_address(teacher):
     assert str(teacher) == "Victor Hugo (23 ans), arrivé(e) le 2023-09-04"
