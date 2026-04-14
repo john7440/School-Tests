24d25868275bce8a9477ec59d4a73299f0625d1c
Jonathan M. <neodev7440@gmail.com>
2026-04-14 13:44:13 +0200

    ajout test pour add course

--- ecole/tests/models/test_student.py
+++ ecole/tests/models/test_student.py
@@ -15,9 +15,15 @@ def course():
     mock.students_taking_it =[]
     return mock
 
+#---------------test add course-------------------
+def test_add_course_should_add_course_to_student(student, course):
+    student.add_course(course)
+    assert course in student.courses_taken
+
 #------------------test str----------------------
-def test_should_contains_first_name_and_last_name(student):
+def test_should_contains_first_name_and_last_name_and_age(student):
     assert "Paul" in str(student)
     assert "Dubois" in str(student)
+    assert "12" in str(student)
 
 
